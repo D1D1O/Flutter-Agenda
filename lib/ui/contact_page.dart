01e0e675904f075b2c9181bef4ff9c7a1f8f73f1
Giovanny Lima da Silva <didiosilva395@gmail.com>
2020-01-30 22:33:04 -0400

    adicionado campos de nome , email e numero . junto com funcionalidades

--- lib/ui/contact_page.dart
+++ lib/ui/contact_page.dart
@@ -1,9 +1,10 @@
 import 'package:agendacontatos/helpers/contact_helper.dart';
 import 'package:flutter/material.dart';
+import 'dart:io';
 
 class ContactPage extends StatefulWidget {
-
   final Contact contact;
+
   ContactPage({this.contact});
 
   @override
@@ -11,34 +12,88 @@ class ContactPage extends StatefulWidget {
 }
 
 class _ContactPageState extends State<ContactPage> {
-
   Contact _editedContact;
+  bool _userEdited = false;
 
+  final _nameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _phoneController = TextEditingController();
 
   @override
   void initState() {
     super.initState();
 
-    if(widget.contact == null){
+    if (widget.contact == null) {
       _editedContact = Contact();
-    }else{
+    } else {
       _editedContact = Contact.fromMap(widget.contact.toMap());
+
+      _nameController.text = _editedContact.name;
+      _emailController.text = _editedContact.email;
+      _phoneController.text = _editedContact.phone;
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(_editedContact ?? "Novo Contato"),
-        backgroundColor: Colors.red,
-        centerTitle: true,
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){},
-        child: Icon(Icons.save),
-        backgroundColor: Colors.red,
-      ),
-    );
+        appBar: AppBar(
+          title: Text(_editedContact.name ?? "Novo Contato"),
+          backgroundColor: Colors.red,
+          centerTitle: true,
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(Icons.save),
+          backgroundColor: Colors.red,
+        ),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Column(
+              children: <Widget>[
+                GestureDetector(
+                  child: Container(
+                      width: 140.0,
+                      height: 140.0,
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          image: DecorationImage(
+                              image: _editedContact.img != null
+                                  ? FileImage(File(_editedContact.img))
+                                  : AssetImage("images/person.png")))),
+                ),
+                TextField(
+                  controller: _nameController,
+                  decoration: InputDecoration(labelText: "Nome"),
+                  onChanged: (text) {
+                    _userEdited = true;
+                    setState(() {
+                      _editedContact.name = text;
+                    });
+                  },
+                ),
+                TextField(
+                  controller: _emailController,
+                  decoration: InputDecoration(labelText: "Email"),
+                  onChanged: (text){
+                    _userEdited  = true;
+                    _editedContact.email = text;
+                  },
+                  keyboardType: TextInputType.emailAddress,
+                ),
+                TextField(
+                  controller: _phoneController,
+                  decoration: InputDecoration(labelText: "Phone"),
+                  onChanged: (text){
+                    _userEdited = true;
+                    _editedContact.phone = text;
+                  },
+                  keyboardType: TextInputType.phone,
+                )
+              ],
+            ),
+          ),
+        ));
   }
 }
